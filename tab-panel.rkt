0b4b3c8f62a9f6b16d79497761222d32824fe95f
Stanley Beef <stanleybeef@gmail.com>
2014-07-10 17:31:06 -0400

    have a function that builds a list of panels, and a list of fields

--- tab-panel.rkt
+++ tab-panel.rkt
@@ -11,11 +11,14 @@
 ;   list : any list
 ; constructs a number of panels equal to the length of the list, returns the panels in a list
 (define build-panels
- (lambda (parent-container lat)
-   (letrec ([f (lambda (l)
-                 (cond
-                   [(null? l) '()]
-                   [(cons (new panel$ [parent parent-container])
-                          (f (cdr l)))]))])
-     (f lat))))
+ (lambda (parent-container l)
+   (map (lambda (e) (new panel% [parent parent-container])) l)))
+
+; (build-fields parent list-of field-info) -> list-of field objects
+;   parent : container?
+;   field-info : ('field-symbol "field name)
+; constructs text fields for each field info thing
+(define build-fields
+  (lambda (parent-container field-list)
+    (map (lambda (e) (new text-field% [parent parent-container] [label (cadr e)])) field-list)))
    
